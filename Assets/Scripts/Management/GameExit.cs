ff9d3033929bc3f3ca073af88eb380cdd40c77d1
Viktor <risider@yandex.ru>
2020-12-12 22:31:21 +0300

    GameExit

---
+++ Assets/Scripts/Management/GameExit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Management
+{
+    internal sealed class GameExit : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
